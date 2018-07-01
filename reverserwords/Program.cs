b677d2ca6344ce9c5900fe98c841034c98e8f464
Kelcey Korngay <kelcey.kornegay@gmail.com>
2018-07-01 13:12:27 -0400

    commit
    sentence with the words in reverse order

--- reverserwords/Program.cs
+++ reverserwords/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+// Input: string – sentence consisting of words separated by spaces
+// output: string – output the sentence with the words in reverse order
 
 namespace reverserwords
 {
@@ -10,6 +12,29 @@ namespace reverserwords
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Please enter sentence");
+            String data =  Console.ReadLine();
+            Console.WriteLine(data);
+
+            reverseorder.reverserword(data);
+        }
+    }
+    public class reverseorder
+    {
+       
+
+        public static void reverserword(string s)
+        {
+            StringBuilder sb = new StringBuilder();
+            string[] words = s.Split(' ');
+
+            for(int i = words.Length -1; i >= 0; i--)
+            {
+                sb.Append(words[i]);
+                sb.Append(" ");
+            }
+           
+            Console.WriteLine(sb);
         }
     }
 }
